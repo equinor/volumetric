5eac24eb651d1af43607db880b5f13a057c24964
Jostein Aune Solaas (IT SI SIT) <josas@lt-lx947112.client.statoil.net>
2018-04-11 15:34:41 +0200

    feat: initial create-react-app setup

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node:8.11.1-alpine
+
+WORKDIR /code
+
+COPY package.json package-lock.json ./
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 3000
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Volumetric</h1>
+        </header>
+        <p className="App-intro">
+          Vælkommin t volumetric.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
