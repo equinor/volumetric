fd4d12a61819f1dc7895c94ffd4f21d64a3f1764
josas <josas@statoil.com>
2018-04-18 15:44:33 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-const client = new ApolloClient();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
