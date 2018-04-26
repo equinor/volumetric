081189ce3005f0032c5faa14189c7d8b294af900
Jostein Solaas <josas@statoil.com>
2018-04-26 10:46:49 +0200

    feat: use sqlalchemy when importing data

--- models/metric.py
+++ models/metric.py
@@ -16,6 +16,11 @@ class Metric(db.Model):
     hcpv = Column(Numeric(20, 2))
     stoiip = Column(Numeric(20, 2))
 
+    def __init__(self, **kwargs):
+        super(Metric, self).__init__(**kwargs)
+        # This keeps the default constructor behavior and is only
+        # necessary if we want to do something else in the constructor
+
     def __repr__(self):
         return "<Metrics(id={id}, model={model}, realization={realization}, faultblock={faultblock}, zone={zone}, grv={grv}, nrv={nrv}, npv={npv}, hcpv={hcpv}, stoiip={stoiip})".format(
             id=self.id, model=self.model, realization=self.realization, faultblock=self.faultblock, zone=self.zone,
