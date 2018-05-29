4e46974a86cc2a234f46cfcf55425360f5191085
Jostein Solaas <josas@statoil.com>
2018-05-29 12:17:18 +0200

    feat: added filtering to model query

--- graphqlapi/schema.py
+++ graphqlapi/schema.py
@@ -1,7 +1,19 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from models import Model as ModelModel
+from models import Model as ModelModel, Faultblock as FaultblockModel, Zone as ZoneModel
+
+
+class Faultblock(SQLAlchemyObjectType):
+    class Meta:
+        model = FaultblockModel
+        interfaces = (relay.Node, )
+
+
+class Zone(SQLAlchemyObjectType):
+    class Meta:
+        model = ZoneModel
+        interfaces = (relay.Node, )
 
 
 class Model(SQLAlchemyObjectType):
@@ -10,13 +22,21 @@ class Model(SQLAlchemyObjectType):
         interfaces = (relay.Node, )
 
 
+class ModelConnectionField(SQLAlchemyConnectionField):
+    RELAY_ARGS = ['first', 'last', 'before', 'after']
+
+    @classmethod
+    def get_query(cls, model, info, **args):
+        query = super(ModelConnectionField, cls).get_query(model, info, **args)
+        for field, value in args.items():
+            if field not in cls.RELAY_ARGS:
+                query = query.filter(getattr(model, field) == value)
+        return query
+
+
 class Query(graphene.ObjectType):
-    hello = graphene.String()
 
-    def resolve_hello(self, info):
-        return 'Hello world!'
-
-    all_metrics = SQLAlchemyConnectionField(Model)
+    all_metrics = ModelConnectionField(Model, name=graphene.String())
 
 
 schema = graphene.Schema(query=Query)
