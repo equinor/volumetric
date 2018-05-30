0575cb70b4ddcfd9767a7c589d88f818ca473752
stoo <stoo@statoil.com>
2018-05-30 08:28:18 +0200

    Added fields to query location by faultblock, zone, and facies

--- graphqlapi/schema.py
+++ graphqlapi/schema.py
@@ -1,7 +1,31 @@
 import graphene
 from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyObjectType, SQLAlchemyConnectionField
-from models import Model as ModelModel
+from models import Model as ModelModel, Faultblock as FaultblockModel, Zone as ZoneModel, Field as FieldModel, Location as LocationModel, Volumetrics as VolumetricsModel
+
+
+class Field(SQLAlchemyObjectType):
+    class Meta:
+        model = FieldModel
+        interfaces = (relay.Node, )
+
+
+class Faultblock(SQLAlchemyObjectType):
+    class Meta:
+        model = FaultblockModel
+        interfaces = (relay.Node, )
+
+
+class Zone(SQLAlchemyObjectType):
+    class Meta:
+        model = ZoneModel
+        interfaces = (relay.Node, )
+
+
+class Location(SQLAlchemyObjectType):
+    class Meta:
+        model = LocationModel
+        interfaces = (relay.Node, )
 
 
 class Model(SQLAlchemyObjectType):
@@ -10,13 +34,36 @@ class Model(SQLAlchemyObjectType):
         interfaces = (relay.Node, )
 
 
+class Volumetrics(SQLAlchemyObjectType):
+    class Meta:
+        model = VolumetricsModel
+        interfaces = (relay.Node, )
+
+
+class ModelConnectionField(SQLAlchemyConnectionField):
+    RELAY_ARGS = ['first', 'last', 'before', 'after']
+
+    @classmethod
+    def get_query(cls, model, info, **args):
+        query = super(ModelConnectionField, cls).get_query(model, info, **args)
+        for field, value in args.items():
+            if field not in cls.RELAY_ARGS:
+                query = query.filter(getattr(model, field) == value)
+        return query
+
+
 class Query(graphene.ObjectType):
-    hello = graphene.String()
 
-    def resolve_hello(self, info):
-        return 'Hello world!'
-
-    all_metrics = SQLAlchemyConnectionField(Model)
+    models = ModelConnectionField(Model, name=graphene.String(), user=graphene.String())
+    fields = ModelConnectionField(Field, name=graphene.String())
+    location = ModelConnectionField(
+        Location, facies=graphene.String(), faultblock_id=graphene.Int(), zone_id=graphene.Int())
+    #all_fields = ModelConnectionField(Field)
+    #all_faultblocks = ModelConnectionField(Faultblock)
+    #all_zones = ModelConnectionField(Zone)
+    #all_locations = ModelConnectionField(Location)
+    all_models = Model(Model)
+    volumetrics = ModelConnectionField(Volumetrics, name=graphene.String())
 
 
 schema = graphene.Schema(query=Query)
