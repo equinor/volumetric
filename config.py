653a36e8854e310932643a26ed49e38cc956b3e3
Jostein Solaas <josas@statoil.com>
2018-09-14 15:56:02 +0200

    fix: update azure AD clientId + set debug to false

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
+
 class Config(object):
     POSTGRES_HOSTNAME = os.environ.get('DATABASE_HOST', 'db')
     POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
@@ -8,13 +9,13 @@ class Config(object):
     POSTGRES_PORT = os.environ.get('DATABASE_PORT', '5432')
     SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', 'something')
     AZURE_KEYS_URI = 'https://login.microsoftonline.com/common/discovery/v2.0/keys'
-    AZURE_APP_CLIENT_ID = '7366258d-0901-4716-b9aa-4a48c3512f6e'
+    AZURE_APP_CLIENT_ID = 'c049f236-7e38-41ba-be77-287bc1c8543c'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "postgresql://{username}:{password}@{hostname}:{port}/{database}" \
         .format(username=POSTGRES_USER, password=POSTGRES_PASSWORD, hostname=POSTGRES_HOSTNAME,
                 port=POSTGRES_PORT, database=POSTGRES_DB)
 
     # Remote debugging
-    REMOTE_DEBUG = True
+    REMOTE_DEBUG = False
 
     UPLOAD_FOLDER = 'uploads'
