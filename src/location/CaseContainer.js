ee960fb32e07c211d6fb057f2f21642f46dbd685
soofstad <stoo@equinor.com>
2019-04-04 12:16:48 +0200

    use StyledSpinner on CaseLoading

--- src/location/CaseContainer.js
+++ src/location/CaseContainer.js
@@ -3,13 +3,13 @@ import { Query } from 'react-apollo';
 import { GraphqlError, NetworkError } from '../common/ErrorHandling';
 import { GET_CASE } from '../common/Queries';
 import { CaseComponent } from './CaseComponent';
-import { SmallSpinner } from '../common/Spinner';
+import { StyledSpinner } from '../common/Spinner';
 
 const CaseContainer = caseId => {
   return (
     <Query query={GET_CASE} variables={caseId}>
       {({ data, loading, error }) => {
-        if (loading) return <SmallSpinner isLoading={true} />;
+        if (loading) return <StyledSpinner isLoading={true} />;
         if (error)
           return error.networkError ? NetworkError(error) : GraphqlError(error);
         return <CaseComponent {...data.case[0]} key={data.case[0].id} />;
