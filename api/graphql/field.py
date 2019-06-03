e3d99e98e672910000806768839ab33db881ea30
Stig Oskar Myhre Ofstad <stoo@equinor.com>
2019-06-03 11:48:18 +0000

    Add field

--- api/graphql/field.py
+++ api/graphql/field.py
@@ -4,6 +4,7 @@ from graphql import GraphQLError
 from models import db, Field as FieldModel
 from utils.ordering import ordered_case, OrderedList
 from .case import Case
+from .validation_error import ValidationError
 
 
 class Field(graphene.ObjectType):
@@ -15,12 +16,17 @@ class Field(graphene.ObjectType):
         return self.cases
 
 
+def validate_field_creation(name):
+    if not FieldModel.query.filter(FieldModel.name == name).first():
+        return True
+
+
 class AddField(graphene.Mutation):
     class Arguments:
         name = graphene.String()
 
-    ok = graphene.Boolean()
     field = graphene.Field(lambda: Field)
+    error = graphene.Field(ValidationError)
 
     def mutate(self, info, name):
         user = info.context.user
@@ -28,8 +34,11 @@ class AddField(graphene.Mutation):
         if not user.isAdmin:
             raise GraphQLError('Unauthorized')
 
+        if not validate_field_creation(name):
+            return AddField(field=Field(name=name), error=ValidationError(id=1, message='The field already exists'))
+
         field = FieldModel(name=name)
         db.session.add(field)
         db.session.commit()
-        ok = True
-        return AddField(field=field, ok=ok)
+
+        return AddField(field=Field(name=name), error=ValidationError(id=0, message=''))
