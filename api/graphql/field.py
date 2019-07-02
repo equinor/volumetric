1377ca41fdb9d08053f8126e0e05b9e0a5c32b57
Jostein Solaas <josas@statoil.com>
2019-07-02 08:58:21 +0200

    fix: return roles from add field resolver

--- api/graphql/field.py
+++ api/graphql/field.py
@@ -2,6 +2,7 @@ import graphene
 from graphql import GraphQLError
 from sqlalchemy import exc
 
+from api.graphql.role import Role
 from models import db, Field as FieldModel, Role as RoleModel
 from utils.ordering import ordered_case, OrderedList
 from .case import Case
@@ -22,6 +23,7 @@ class AddField(graphene.Mutation):
         name = graphene.String()
 
     field = graphene.Field(lambda: Field)
+    roles = graphene.List(Role)
     error = graphene.Field(ValidationError)
 
     def mutate(self, info, name):
@@ -29,15 +31,14 @@ class AddField(graphene.Mutation):
         # Only admins are allowed to create new fields
         if not user.isAdmin:
             raise GraphQLError('Unauthorized')
-
         try:
             field = FieldModel(name=name)
             role = RoleModel(user=user.shortname, role="fieldadmin", field=name)
             db.session.add(field)
-            db.session.commit()
+            db.session.flush()
             db.session.add(role)
             db.session.commit()
         except exc.IntegrityError:
             return AddField(field=Field(name=name), error=ValidationError(id=1, message='The field already exists'))
-
-        return AddField(field=Field(name=name), error=ValidationError(id=0, message=''))
+        roles = RoleModel.query.filter(RoleModel.user == user.shortname)
+        return AddField(field=Field(name=name), roles=roles, error=ValidationError(id=0, message=''))
