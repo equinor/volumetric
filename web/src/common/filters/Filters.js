dd6b43e40966ad7a425f550a1ce5f61331d41d50
Jostein Solaas <josas@statoil.com>
2019-07-12 15:51:54 +0200

    feat: show totals on location filters

--- web/src/common/filters/Filters.js
+++ web/src/common/filters/Filters.js
@@ -1,12 +1,69 @@
 import React from 'react';
 import FilterList from './FilterList';
 import { H4 } from '../Headers';
+import styled from 'styled-components';
+import { LINK_COLOR } from '../variables';
+
+const ShowTotalsButton = styled.button`
+  font-family: Equinor-Medium, serif;
+  font-weight: 500;
+  color: ${LINK_COLOR};
+
+  // Button as link style
+  background: none;
+  border: none;
+  padding: 0 !important;
+  /*border is optional*/
+  text-decoration: underline solid ${LINK_COLOR};
+  cursor: pointer;
+  height: fit-content;
+  margin-left: 35px;
+
+  :focus {
+    outline: none;
+  }
+`;
+
+const ShowTotals = ({ filters, checked, setFilterState, category }) => {
+  const showingTotals = filters.every(filter => checked.includes(filter));
+  if (showingTotals) {
+    return (
+      <ShowTotalsButton onClick={() => setFilterState(category, [])}>
+        Clear
+      </ShowTotalsButton>
+    );
+  }
+  return (
+    <ShowTotalsButton onClick={() => setFilterState(category, filters)}>
+      Show totals
+    </ShowTotalsButton>
+  );
+};
 
 export const Filter = props => {
-  const { name, category, filters, handleFilterChange, checked } = props;
+  const {
+    name,
+    category,
+    filters,
+    handleFilterChange,
+    setFilterState,
+    showTotals = false,
+    checked,
+  } = props;
+
   return (
     <div>
-      <H4>{name}</H4>
+      <div style={{ display: 'flex', alignItems: 'baseline' }}>
+        <H4>{name}</H4>
+        {showTotals && (
+          <ShowTotals
+            category={category}
+            filters={filters}
+            checked={checked}
+            setFilterState={setFilterState}
+          />
+        )}
+      </div>
       <FilterList
         filters={filters}
         handleFilterChange={handleFilterChange}
--- web/src/common/variables.js
+++ web/src/common/variables.js
@@ -1,5 +1,6 @@
 // export const PRIMARY_COLOR = 'rgb(18, 147, 154)';
 export const PRIMARY_COLOR = '#007079';
+export const LINK_COLOR = '#445786';
 export const DEFAULT_COLOR = 'white';
 export const HOVER_COLOR = 'gray';
 export const ALMOST_BLACK = '#363636';
@@ -14,13 +15,11 @@ export const DANGER_COLOR = '#db3b21';
 export const WARNING_COLOR = '#bf4265';
 export const LIST_SEPARATOR_COLOR = '#f0f0f0';
 
-/*
-const colorPalette = [
-  '#007079',
-  '#D5EAF4',
-  '#243746',
-  '#FFE7D6',
-  '#7d0023',
-  '#E6FAEC',
-];
-*/
+// const colorPalette = [
+//   '#007079',
+//   '#D5EAF4',
+//   '#243746',
+//   '#FFE7D6',
+//   '#7d0023',
+//   '#E6FAEC',
+// ];
